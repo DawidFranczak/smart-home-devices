7c317183eda84ee4dae0bb899f489c588eda61a4
DawidFranczak <daw.franczak@gmail.com>
2025-09-19 12:37:14 +0200

    Switch communication from TCP to MQTT and restore functionality

--- arduino/button_esp8266_01_01M/src/button.cpp
+++ arduino/button_esp8266_01_01M/src/button.cpp
@@ -1,13 +1,14 @@
 #include <Arduino.h>
+#include <Mqtt.h>
+#include <ButtonMessage.h>
 #include "button.h"
 
-Button::Button(int input_pin, CommunicationModule& communication_module)
-  : button_pin(input_pin), communication_module(communication_module) {
+Button::Button(int input_pin, Mqtt& mqtt)
+  : button_pin(input_pin), mqtt(mqtt) {
   pinMode(button_pin, INPUT_PULLUP);
 }
 
-void Button::start() {
-  check_message();
+void Button::loop() {
   check_button();
 }
 
@@ -17,13 +18,11 @@ void Button::check_button() {
   static bool sended = false;
 
   if (press_time > 1000 && !sended) {
+    mqtt.sendMessage(onHoldRequest(mqtt.getMac()));
     sended = true;
-    DeviceMessage message = on_hold_request(communication_module.get_mac());
-    communication_module.send_message(message);
     press_time = 0;
   } else if (press_time > 30 && press_time < 1000 and digitalRead(button_pin) == HIGH){
-    DeviceMessage message = on_click_request(communication_module.get_mac());
-    communication_module.send_message(message);
+    mqtt.sendMessage(onClickRequest(mqtt.getMac()));
     press_time = 0;
   }
 
@@ -34,21 +33,4 @@ void Button::check_button() {
   }else {
     press_time = millis() - last_check;
   }
-}
-
-void Button::check_message() {
-  DeviceMessage* message = communication_module.get_message();
-  if (!message) return;
-  String method_name = communication_module.extract_method_name_from_message(*message);
-  if (method_name == "_set_settings_response") {
-    communication_module.send_message(set_settings_response(*message));
-  }else{
-  }
-  delete message;
-}
-
-DeviceMessage Button::set_settings_response(DeviceMessage& message) {
-  JsonDocument payload;
-  payload["status"] = "accept";
-  return DeviceMessage(message.message_id, message.message_event, "response", message.device_id, payload);
 }
--- arduino/button_esp8266_01_01M/src/main.cpp
+++ arduino/button_esp8266_01_01M/src/main.cpp
@@ -1,15 +1,19 @@
 #include <Arduino.h>
-#include "CommunicationProtocol/communication_module.h"
+#include <Mqtt.h>
 #include "button.h"
+#include "settings.h"
 
-CommunicationModule communication_module;
-Button button(2, communication_module);
+Mqtt mqtt(BROKER_IP, BROKER_PORT, BROKER_NAME, SSID, PASSWORD, DEVICE_FUNCTION, HEALT_CHECK_INTERVAL);
+Button button(BUTTON_PIN, mqtt);
 
 void setup() {
+  // Serial.begin(9600);
+  mqtt.begin();
+  mqtt.onMessage([](Message msg) {
+  });
 }
 
 void loop() {
-  communication_module.start();
-  button.start();
+  button.loop();
   delay(10);
 }

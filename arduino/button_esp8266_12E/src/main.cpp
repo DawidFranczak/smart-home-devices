a1467c6b3431f5dc826368e95e96df317c89ba67
DawidFranczak <daw.franczak@gmail.com>
2025-09-19 12:33:20 +0200

    Switch communication from TCP to MQTT and restore functionality

--- arduino/button_esp8266_12E/src/main.cpp
+++ arduino/button_esp8266_12E/src/main.cpp
@@ -4,7 +4,7 @@
 #include "settings.h"
 
 Mqtt mqtt(BROKER_IP, BROKER_PORT, BROKER_NAME, SSID, PASSWORD, DEVICE_FUNCTION, HEALT_CHECK_INTERVAL);
-Button button(2, mqtt);
+Button button(BUTTON_PIN, mqtt);
 
 void setup() {
   // Serial.begin(9600);
